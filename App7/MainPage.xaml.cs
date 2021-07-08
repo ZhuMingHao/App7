ce92b14f1316410e6005758b5b81120846be904e
朱明浩 <minghao908@hotmail.com>
2021-07-09 01:01:21 +0800

    添加项目文件。

---
+++ App7/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace App7
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            SharedShadow.Receivers.Add(BackgroundGrid);
+            MyButton.Translation += new Vector3(-30, -30, 30);
+
+        }
+
+        Compositor _compositor = Window.Current.Compositor;
+        SpringVector3NaturalMotionAnimation _springAnimation;
+
+        private void CreateOrUpdateSpringAnimation(float finalValue)
+        {
+            if (_springAnimation == null)
+            {
+                _springAnimation = _compositor.CreateSpringVector3Animation();
+                _springAnimation.Target = "Scale";
+            }
+
+            _springAnimation.FinalValue = new Vector3(finalValue);
+        }
+
+        private void element_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            // Scale up to 1.5
+            CreateOrUpdateSpringAnimation(1.5f);
+            (sender as UIElement).CenterPoint = new Vector3((float)(MyButton.ActualWidth / 2.0), (float)(MyButton.ActualHeight / 2.0), 1f);
+            (sender as UIElement).StartAnimation(_springAnimation);
+        }
+
+        private void element_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            // Scale back down to 1.0
+            CreateOrUpdateSpringAnimation(1.0f);
+            (sender as UIElement).CenterPoint = new Vector3((float)(MyButton.ActualWidth / 2.0), (float)(MyButton.ActualHeight / 2.0), 1f);
+            (sender as UIElement).StartAnimation(_springAnimation);
+        }
+    }
+}
